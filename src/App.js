59710383f407930555305f5feb4c0a47a46a4f63
EJinK0 <wls5819@naver.com>
2023-12-20 00:14:34 +0900

    [ADD] api호출 Test버튼 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axioswrapper from './utils/axios';
 
 function App() {
+  const [myApiResult, setMyApiResult] = useState('');
+
+  const apiCall = () => {
+    const payload = {
+        id: 'e380f6f9-b1a2-4b1f-83aa-82bd870dabd2',
+        name: 'kim',
+        email: 'kim@bb.bb'
+      };
+
+    axioswrapper('POST', 'http://localhost:8080/users', payload)
+    .then((response) => {
+        console.log('call success', response);
+        setMyApiResult(JSON.stringify(response));
+    })
+    .catch((error) => {
+        console.log('call fail', error);
+    });
+};
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +31,8 @@ function App() {
           heyyyyyyyyyyyyyy2222<br/>
           this is dev server
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={apiCall}>api 호출하기</button>
+        {myApiResult}
       </header>
     </div>
   );
